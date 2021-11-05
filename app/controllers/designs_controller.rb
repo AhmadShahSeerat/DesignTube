4b6d4d17e50a1ab2e69708350c6ecaf00db29956
iamShah <formal>
2021-11-05 09:06:49 -0400

    second one

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -101,10 +101,4 @@ class DesignsController < ApplicationController
 end
 
 
-# echo "# DesignTube" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin git@github.com:AhmadShahSeerat/DesignTube.git
-# git push -u origin main
+#last one 
